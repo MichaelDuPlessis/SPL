d4cf14b86bd284f1603dbb1d23a815a37009ea95
Michael du Plessis <mdp0101@gmail.com>
2022-05-17 22:12:39 +0200

    removed unnecessary code

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,25 @@
-use std::{fs, time::Instant, rc::Rc};
-
-use crate::scope::ScopeAnalysis;
+use std::{fs, rc::Rc};
 
 mod lexer;
 mod parser;
 mod grammer;
 mod token;
 mod stack;
-mod scope;
 
 fn main() {
-    // let mut input = String::new();
-    // std::io::stdin().read_line(&mut input).unwrap();
-    // let input = &input[..input.len() - 1];
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let input = &input[..input.len() - 1];
 
-    let start = Instant::now();
-
-    let file = match fs::read_to_string("./test.spl") {
+    let file = match fs::read_to_string(input) {
         Ok(f) => f,
         Err(e) => panic!("{}", e),
     };
 
     let mut lexer = lexer::Lexer::new(&file);
     let tokens = lexer.tokenize();
-    // println!("{:?}", tokens);
 
     let parser = parser::Parser::new(tokens);
     let node = parser.parse();
     parser::Parser::create_xml(Rc::clone(&node));
-
-    let mut scope = ScopeAnalysis::new(node);
-    println!("{:?}", scope.scope());
-
-    println!("{:?}", start.elapsed());
 }
